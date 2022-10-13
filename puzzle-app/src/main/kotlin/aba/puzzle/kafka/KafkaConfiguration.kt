3bc254550175d37cca83284d696c8d5ab6f66078
aleksey.barabanov <alexei.barabanov@gmail.com>
2022-10-13 15:43:58 +0600

    logbacked added, dynamic kafka consumer added.

--- puzzle-app/src/main/kotlin/aba/puzzle/kafka/KafkaConfiguration.kt
+++ puzzle-app/src/main/kotlin/aba/puzzle/kafka/KafkaConfiguration.kt
@@ -2,22 +2,38 @@ package aba.puzzle.kafka
 
 import aba.puzzle.domain.dto.PuzzleStateVO
 import io.confluent.kafka.serializers.KafkaJsonSerializer
+import mu.KotlinLogging
 import org.apache.kafka.clients.admin.AdminClientConfig
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.BeanFactory
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.KafkaListenerContainerFactory
+import org.springframework.kafka.config.KafkaListenerEndpoint
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry
+import org.springframework.kafka.config.MethodKafkaListenerEndpoint
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaAdmin
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.listener.MessageListener
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory
+import org.springframework.stereotype.Component
+import java.util.*
+import java.util.concurrent.CompletableFuture
 
 
 @Configuration
 class KafkaConfiguration {
+    private val log = KotlinLogging.logger {}
+
     @Value("\${app.kafka.bootstrap}")
     lateinit var bootstrapAddress: String
 
@@ -26,7 +42,7 @@ class KafkaConfiguration {
 
     @Bean("producerFactory")
     fun producerFactory(): ProducerFactory<String, PuzzleStateVO> {
-        println("kafka bootstrap $bootstrapAddress")
+        log.info { "kafka bootstrap $bootstrapAddress" }
         val configProps: MutableMap<String, Any> = HashMap()
         configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapAddress
         configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
@@ -38,9 +54,10 @@ class KafkaConfiguration {
     fun kafkaTemplate(@Autowired @Qualifier("producerFactory") factory: ProducerFactory<String, PuzzleStateVO>): KafkaTemplate<String, PuzzleStateVO> {
         return KafkaTemplate(factory)
     }
+
     @Bean("producerFactoryString")
     fun producerFactoryString(): ProducerFactory<String, String> {
-        println("kafka bootstrap $bootstrapAddress")
+        log.info { "kafka bootstrap $bootstrapAddress" }
         val configProps: MutableMap<String, Any> = HashMap()
         configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapAddress
         configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
@@ -51,5 +68,95 @@ class KafkaConfiguration {
     @Bean
     fun kafkaTemplateString(@Autowired @Qualifier("producerFactoryString") factory: ProducerFactory<String, String>): KafkaTemplate<String, String> {
         return KafkaTemplate(factory)
+    }
+}
+
+abstract class CustomMessageListener {
+    abstract fun createKafkaListenerEndpoint(name: String, topic: String, groupId: String): KafkaListenerEndpoint
+    protected fun createDefaultMethodKafkaListenerEndpoint(
+        name: String,
+        topic: String,
+        consumerGroupId: String
+    ): MethodKafkaListenerEndpoint<String, String> {
+        val props = Properties()
+        props[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+        return with (MethodKafkaListenerEndpoint<String, String>()) {
+            setId(getConsumerId(name))
+            setGroupId(consumerGroupId)
+            setAutoStartup(true)
+            setTopics(topic)
+            setConsumerProperties(props)
+            setMessageHandlerMethodFactory(DefaultMessageHandlerMethodFactory())
+            this
+        }
+    }
+
+    private fun getConsumerId(name: String): String {
+        return if (isBlank(name)) {
+            CustomMessageListener::class.java.canonicalName + "#" + NUMBER_OF_LISTENERS++
+        } else {
+            name
+        }
+    }
+
+    private fun isBlank(string: String?): Boolean {
+        return string.isNullOrBlank()
+    }
+
+    companion object {
+        private var NUMBER_OF_LISTENERS = 0
+    }
+}
+
+@Component
+class MyCustomMessageListener : CustomMessageListener() {
+    override fun createKafkaListenerEndpoint(name: String, topic: String, groupId: String): KafkaListenerEndpoint {
+        val kafkaListenerEndpoint = createDefaultMethodKafkaListenerEndpoint(name, topic, groupId)
+        kafkaListenerEndpoint.bean = MyMessageListener()
+        kafkaListenerEndpoint.method = MyMessageListener::class.java.getMethod(
+            "onMessage", ConsumerRecord::class.java
+        )
+        return kafkaListenerEndpoint
+    }
+
+    private class MyMessageListener : MessageListener<String, String> {
+        private val log = KotlinLogging.logger {}
+        override fun onMessage(record: ConsumerRecord<String, String>) {
+            log.info { "My message listener got a new record: $record" }
+            CompletableFuture.runAsync { sleep() }.join()
+            log.info { "My message listener done processing record: $record" }
+        }
+
+        private fun sleep() {
+            Thread.sleep(100)
+        }
+    }
+}
+
+@Component
+class CustomKafkaListenerRegistrar {
+    private val log = KotlinLogging.logger {}
+
+    @Autowired
+    private lateinit var beanFactory: BeanFactory
+
+    @Autowired
+    private lateinit var kafkaListenerEndpointRegistry: KafkaListenerEndpointRegistry
+
+    @Autowired
+    private lateinit var kafkaListenerContainerFactory: KafkaListenerContainerFactory<*>
+
+    fun registerCustomKafkaListener(name: String, topic: String, groupId: String, startImmediately: Boolean) {
+        kotlin.runCatching {
+            val listenerClass = MyCustomMessageListener::class.java
+            val customMessageListener =
+                beanFactory.getBean(Class.forName(listenerClass.canonicalName)) as CustomMessageListener
+            kafkaListenerEndpointRegistry.registerListenerContainer(
+                customMessageListener.createKafkaListenerEndpoint(name, topic, groupId),
+                kafkaListenerContainerFactory, startImmediately
+            )
+        }.onFailure {
+            log.error(it) { "Exception in bean creation"}
+        }
     }
 }
--- puzzle-app/src/main/kotlin/aba/puzzle/service/LaunchService.kt
+++ puzzle-app/src/main/kotlin/aba/puzzle/service/LaunchService.kt
@@ -5,6 +5,8 @@ import aba.puzzle.domain.PuzzlePosition
 import aba.puzzle.domain.PuzzleState
 import aba.puzzle.domain.dto.DetailVO
 import aba.puzzle.domain.dto.PuzzleStateVO
+import aba.puzzle.kafka.CustomKafkaListenerRegistrar
+import mu.KotlinLogging
 import org.apache.kafka.clients.admin.AdminClient
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Autowired
@@ -28,8 +30,10 @@ class LaunchServiceImpl(
     @Autowired val kafkaProducer: KafkaTemplate<String, PuzzleStateVO>,
     @Autowired val kafkaTaskTopicsProducer: KafkaTemplate<String, String>,
     @Autowired val kafkaAdmin: KafkaAdmin,
+    @Autowired val listenerRegistrar: CustomKafkaListenerRegistrar,
 ) : LaunchService, InitializingBean {
     lateinit var allDetails: Collection<Detail>
+    private val log = KotlinLogging.logger {}
 
     override fun launch(topic: String): Boolean {
         //check that the task is not already running. Make call to repository for the answer
@@ -39,6 +43,9 @@ class LaunchServiceImpl(
 
         //publish topic for consumers to subscribe
         sendNewTopic(topic)
+
+        //subscribe on the new topic
+        listenerRegistrar.registerCustomKafkaListener(topic, topic, "defaultGroup", true)
 
         //Take each detail and put it on the left upper corner of the puzzle with all possible rotations (4).
         allDetails.forEach {
@@ -65,27 +72,27 @@ class LaunchServiceImpl(
     private fun sendNewTopic(topic: String) {
         val taskTopicsFuture = kafkaTaskTopicsProducer.send(taskTopicsTopic, topic)
         taskTopicsFuture.addCallback({
-            println("message sent to $taskTopicsTopic")
+            log.info { "message sent to $taskTopicsTopic" }
         }, {
-            println("Exception in sending to kafka topic $taskTopicsTopic $it")
+            log.info { "Exception in sending to kafka topic $taskTopicsTopic $it" }
         })
     }
 
     private fun sendPuzzles(topic: String, puzzleState: PuzzleState) {
         val puzzleFuture = kafkaProducer.send(topic, PuzzleStateVO.fromPuzzleState(puzzleState))
         puzzleFuture.addCallback({
-            println("message sent to $topic")
+            log.info {"message sent to $topic"}
         }, {
-            println("Exception in sending to kafka $it")
+            log.info {"Exception in sending to kafka $it"}
         })
     }
 
     override fun afterPropertiesSet() {
-        println("repository url: $repositoryUrl")
+        log.info { "repository url: $repositoryUrl" }
         val detailList = WebClient.create(repositoryUrl).get().uri("/details")
             .accept(MediaType.APPLICATION_JSON).retrieve().bodyToFlux(DetailVO::class.java)
             .map { DetailVO.toDetail(it) }.collectList().block()
         allDetails = detailList!!.toList()
-        println("rs: $allDetails")
+        log.info { "rs: $allDetails" }
     }
 }
