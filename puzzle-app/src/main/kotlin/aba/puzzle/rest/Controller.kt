b844a67050c5856bce24f9787a64b0e95a925270
aleksey.barabanov <alexei.barabanov@gmail.com>
2022-10-13 12:06:10 +0600

    topic per task

--- puzzle-app/src/main/kotlin/aba/puzzle/rest/Controller.kt
+++ puzzle-app/src/main/kotlin/aba/puzzle/rest/Controller.kt
@@ -2,7 +2,9 @@ package aba.puzzle.rest
 
 import aba.puzzle.service.LaunchService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -11,26 +13,36 @@ import org.springframework.web.bind.annotation.*
 class Controller(@Autowired val launchService: LaunchService) {
 
     @GetMapping("/info")
-    fun info() : String {
+    fun info(): String {
         return "test"
     }
 
-    @PostMapping("/run")
-    fun run() {
-        if (launchService.launch()) {
-            return
+    @PostMapping("/run", consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun run(@RequestBody launchRequest: LaunchRequest): LaunchResponse {
+        val topicName = "puzzle-${launchRequest.testId}"
+        if (launchService.launch(topicName)) {
+            return LaunchResponse(launched = true, topicName = topicName)
         } else {
-            throw AlreadyRunningException()
+            throw AlreadyRunningException(topicName=topicName)
         }
     }
 }
 
-class AlreadyRunningException: RuntimeException()
+class AlreadyRunningException(val topicName: String) : RuntimeException()
 
 @ControllerAdvice
 class AlreadyRunningExceptionController {
     @ExceptionHandler(value = [AlreadyRunningException::class])
-    fun exception(exception: AlreadyRunningException): ResponseEntity<Any> {
-        return ResponseEntity("Task is already running", HttpStatus.INTERNAL_SERVER_ERROR)
+    fun exception(exception: AlreadyRunningException): ResponseEntity<LaunchResponse> {
+        return ResponseEntity(LaunchResponse(launched = false, topicName = exception.topicName), HttpStatus.INTERNAL_SERVER_ERROR)
     }
 }
+
+class LaunchRequest {
+    val testId: String = "defaultTest"
+}
+
+data class LaunchResponse(
+    val launched: Boolean,
+    val topicName: String
+)
--- puzzle-app/src/main/kotlin/aba/puzzle/service/LaunchService.kt
+++ puzzle-app/src/main/kotlin/aba/puzzle/service/LaunchService.kt
@@ -5,6 +5,7 @@ import aba.puzzle.domain.PuzzlePosition
 import aba.puzzle.domain.PuzzleState
 import aba.puzzle.domain.dto.DetailVO
 import aba.puzzle.domain.dto.PuzzleStateVO
+import aba.puzzle.rest.LaunchRequest
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
@@ -14,19 +15,18 @@ import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 
 interface LaunchService {
-    fun launch(): Boolean
+    fun launch(topic: String): Boolean
 }
 
 @Component
 class LaunchServiceImpl(
     @Value("\${app.repositoryUrl}") val repositoryUrl: String,
-    @Value("\${app.kafka.topic.puzzlestate}") val puzzleStateTopic: String,
     @Autowired val puzzleStateService: PuzzleStateService,
     @Autowired val kafkaProducer: KafkaTemplate<String, PuzzleStateVO>
 ) : LaunchService, InitializingBean {
     lateinit var allDetails: Collection<Detail>
 
-    override fun launch(): Boolean {
+    override fun launch(topic: String): Boolean {
         //TODO: check that the task is not already running. Make call to repository for the answer
         var isAlreayRunning = false
 
@@ -37,7 +37,7 @@ class LaunchServiceImpl(
         //Take each detail and put it on the left upper corner of the puzzle with all possible rotations (4).
         allDetails.forEach {
             puzzleStateService.addDetail(PuzzleState(), it, PuzzlePosition.left_upper).forEach {
-                val f = kafkaProducer.send(puzzleStateTopic, PuzzleStateVO.fromPuzzleState(it))
+                val f = kafkaProducer.send(topic, PuzzleStateVO.fromPuzzleState(it))
                 f.addCallback({rs -> {
                     println("message sent")
                 }},{
