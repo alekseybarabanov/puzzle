a18e85d4a77c9c8ecad6d06bb660f61e95145dff
aleksey.barabanov <alexei.barabanov@gmail.com>
2022-10-14 16:06:29 +0600

    added spring security and actuator

---
+++ puzzle-api/src/main/kotlin/aba/puzzle/rest/Security.kt
@@ -0,0 +1,45 @@
+package aba.puzzle.rest
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+@EnableWebSecurity
+class SecSecurityConfig {
+
+    @Bean
+    fun userDetailsService(passwordEncoder: PasswordEncoder): InMemoryUserDetailsManager? {
+        val user = User.withUsername("user")
+            .password(passwordEncoder.encode("password"))
+            .roles("USER")
+            .build()
+        return InMemoryUserDetailsManager(user)
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain? {
+        http.authorizeRequests()
+            .antMatchers("/run")
+            .permitAll()
+            .antMatchers("/actuator/**")
+            .permitAll()
+            .antMatchers("/**")
+            .denyAll()
+            .and().csrf().disable()
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder? {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
+    }
+}
