530961c8e4473eb62ed0d961eb5fb43d46673dd6
aleksey.barabanov <alexei.barabanov@gmail.com>
2022-10-17 19:56:14 +0600

    hsql db

--- misc/curls.sh
+++ misc/curls.sh
@@ -3,8 +3,6 @@ curl -XPOST localhost:9500/run -vs -u user:password -d @puzzle3x3.json \
 -H "Content-Type: application/json" \
 -H "Idempotency-Key: 9"
 
-curl localhost:9000/details
-
 curl -XPOST localhost:9000/newPuzzleConfig -d @testPuzzleConfig.json -H "Content-Type: application/json"
 
 curl localhost:9000/puzzleConfig/puzzle-1
--- puzzle-repository-hsqldb/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-insert into COLOR VALUES('red');
-insert into COLOR VALUES('green');
-insert into COLOR VALUES('yellow');
-insert into COLOR VALUES('white');
-
-INSERT INTO BALL_PART VALUES('one_third');
-INSERT INTO BALL_PART VALUES('two_thirds');
