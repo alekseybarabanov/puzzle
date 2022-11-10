c76d4d7d6eed963ef7f50412b4f35f1f07fcac8f
aleksey.barabanov <alexei.barabanov@gmail.com>
2022-11-10 08:52:18 +0300

    Create resources to install apps into k8s cluster #4 added puzzle repository resources to k8s, it's working

--- misc/curls.sh
+++ misc/curls.sh
@@ -18,3 +18,8 @@ curl -XPOST localhost:8400/generate -d @puzzleGen2x2.json -H "Content-Type: appl
 curl -XPOST 10.102.39.220:8080/run -vs -u user:password -d @puzzle3x3.json \
 -H "Content-Type: application/json" \
 -H "Idempotency-Key: 1"
+
+curl -XPOST http://10.96.74.118:8080/newPuzzleConfig -vs -d @testPuzzleConfig.json \
+-H "Content-Type: application/json"
+
+curl http://10.96.74.118:8080/puzzleConfig/puzzle-1
